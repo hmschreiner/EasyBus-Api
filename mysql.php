d90167ccf6a22173ff7d0d3fe8d0ad79c120f2c2
Alexandre Machado <alexandre@machado.cc>
2015-04-11 20:09:38 -0300

    no message

--- mysql.php
+++ mysql.php
@@ -2,6 +2,6 @@
 $vars = array('MYSQL_DATABASE_NAME' ,'MYSQL_PORT' ,'MYSQL_PASSWORD' ,'MYSQL_USER' ,'MYSQL_HOST' ,'TSURU_SERVICES');
 foreach($vars as $i)
 {
-    echo $i . ": " . $_ENV[$i] . '<br>';
+    echo $i . ": " . getenv($i) . '<br>';
 }
 ?>
